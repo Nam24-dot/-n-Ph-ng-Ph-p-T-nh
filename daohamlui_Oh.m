1b55ac1d7456c3cf31fe15e1980df0c0aea2db0e
Nam24-dot <112529881+Nam24-dot@users.noreply.github.com>
2024-12-24 17:25:36 +0700

    Delete daohamlui_Oh.m

--- daohamlui_Oh.m
+++
@@ -1,7 +0,0 @@
-function result = daohamlui_Oh(x_data, y_data, h, x_interpolate)
-    [~, idx] = min(abs(x_data - x_interpolate));
-    if idx == 1
-        error('Giá trị cần tính đạo hàm phải nằm sau điểm đầu tiên.');
-    end
-    result = (y_data(idx) - y_data(idx - 1)) / h;
-end
